34ab933a1f045a95978f204fcfee8cf5d7a5a3f9
angelopez <angelopez@hotelbeds.com>
2018-03-05 12:52:43 +0100

    codigo fizzbuzz con bazzinga :->

--- fizz-buzz/src/main/java/FizzBuzz.java
+++ fizz-buzz/src/main/java/FizzBuzz.java
@@ -4,18 +4,32 @@ import java.util.stream.IntStream;
 
 public class FizzBuzz {
     public List<String> createOneToHundredList() {
-        return IntStream.rangeClosed(1, 100).boxed().map(this::mapMultipleOfThreeToFizzAndMulptipleOfFiveToBuzz).collect(Collectors.toList());
+        return IntStream.rangeClosed(1, 100).mapToObj(this::mapMultipleOfThreeToFizzAndMulptipleOfFiveToBuzz).collect(Collectors.toList());
     }
 
     private String mapMultipleOfThreeToFizzAndMulptipleOfFiveToBuzz(int number) {
-        if (number % 3 == 0 && number % 5 == 0) {
-            return "FizzBuzz";
-        } else if (number % 3 == 0) {
+        if (isFizz(number) && isBuzz(number)) {
+            return "Bazzinga";
+        } else if (isFizz(number)) {
             return "Fizz";
-        } else if (number % 5 == 0) {
+        } else if (isBuzz(number)) {
             return "Buzz";
+        } else {
+            return String.valueOf(number);
         }
-        return String.valueOf(number);
     }
+
+    private boolean isBuzz(int number) {
+        return isMultipleOrContains(number, 5);
+    }
+
+    private boolean isFizz(int number) {
+        return isMultipleOrContains(number, 3);
+    }
+
+    private boolean isMultipleOrContains(int number, int valueToCompare) {
+        return number % valueToCompare == 0 || String.valueOf(number).contains(String.valueOf(valueToCompare));
+    }
+
 }
 
--- fizz-buzz/src/test/java/FizzBuzzTest.java
+++ fizz-buzz/src/test/java/FizzBuzzTest.java
@@ -6,53 +6,79 @@ import org.junit.Test;
 
 public class FizzBuzzTest {
 
+    public static final String ITEM_NOT_IN_LIST = "Item not in list";
+
     @Test
     public void createOneToHundredListTest() {
-        FizzBuzz fizzBuzz = new FizzBuzz();
-        List<String> oneToHundredList = fizzBuzz.createOneToHundredList();
-        assertEquals("Item not in list", 100, oneToHundredList.size());
+        assertEquals(ITEM_NOT_IN_LIST, 100, getListToTest().size());
     }
 
-	@Test
-	public void multiplesThreeFizzTest() {
-		FizzBuzz fizzBuzz = new FizzBuzz();
-		List<String> oneToHundredList = fizzBuzz.createOneToHundredList();
-		assertEquals("Item not in list", "Fizz", oneToHundredList.get(2));
-	}
+    @Test
+    public void multiplesThreeFizzTest() {
+        assertEquals(ITEM_NOT_IN_LIST, "Fizz", obtainValueToTest(3));
+    }
 
-	@Test
-	public void multiplesNineFizzTest() {
-		FizzBuzz fizzBuzz = new FizzBuzz();
-		List<String> oneToHundredList = fizzBuzz.createOneToHundredList();
-		assertEquals("Item not in list", "Fizz", oneToHundredList.get(8));
-	}
+    @Test
+    public void multiplesNineFizzTest() {
+        assertEquals(ITEM_NOT_IN_LIST, "Fizz", obtainValueToTest(9));
+    }
 
-	@Test
-	public void multiplesFiveBuzzTest() {
-		FizzBuzz fizzBuzz = new FizzBuzz();
-		List<String> oneToHundredList = fizzBuzz.createOneToHundredList();
-		assertEquals("Item not in list", "Buzz", oneToHundredList.get(4));
-	}
+    @Test
+    public void multiplesFiveBuzzTest() {
+        assertEquals(ITEM_NOT_IN_LIST, "Buzz", obtainValueToTest(5));
+    }
 
-	@Test
-	public void multiplesTenBuzzTest() {
-		FizzBuzz fizzBuzz = new FizzBuzz();
-		List<String> oneToHundredList = fizzBuzz.createOneToHundredList();
-		assertEquals("Item not in list", "Buzz", oneToHundredList.get(9));
-	}
+    @Test
+    public void multiplesTenBuzzTest() {
+        assertEquals(ITEM_NOT_IN_LIST, "Buzz", obtainValueToTest(10));
+    }
 
-	@Test
-	public void multiplesFiveTeenBuzzTest() {
-		FizzBuzz fizzBuzz = new FizzBuzz();
-		List<String> oneToHundredList = fizzBuzz.createOneToHundredList();
-		assertEquals("Item not in list", "FizzBuzz", oneToHundredList.get(14));
-	}
+    @Test
+    public void multiplesFiveTeenBuzzTest() {
+        assertEquals(ITEM_NOT_IN_LIST, "Bazzinga", obtainValueToTest(15));
+    }
 
-	@Test
-	public void multiplesThirtyTeenBuzzTest() {
-		FizzBuzz fizzBuzz = new FizzBuzz();
-		List<String> oneToHundredList = fizzBuzz.createOneToHundredList();
-		assertEquals("Item not in list", "FizzBuzz", oneToHundredList.get(29));
-	}
+    @Test
+    public void multiplesThirtyTeenBuzzTest() {
+        assertEquals(ITEM_NOT_IN_LIST, "Bazzinga", obtainValueToTest(30));
+    }
 
+    @Test
+    public void containsThreeIsFizzTest() {
+        assertEquals(ITEM_NOT_IN_LIST, "Fizz", obtainValueToTest(31));
+    }
+
+    @Test
+    public void containsFiveIsBuzzTest() {
+        assertEquals(ITEM_NOT_IN_LIST, "Buzz", obtainValueToTest(58));
+    }
+
+    @Test
+    public void containsFiftyOneIsFizzBuzzTest() {
+        assertEquals(ITEM_NOT_IN_LIST, "Bazzinga", obtainValueToTest(51));
+    }
+
+    @Test
+    public void containsFiftyThreeIsFizzBuzzTest() {
+        assertEquals(ITEM_NOT_IN_LIST, "Bazzinga", obtainValueToTest(53));
+    }
+
+    @Test
+    public void containsThirtyFiveIsFizzBuzzTest() {
+        assertEquals(ITEM_NOT_IN_LIST, "Bazzinga", obtainValueToTest(35));
+    }
+
+    @Test
+    public void containsFortyFiveIsFizzBuzzTest() {
+        assertEquals(ITEM_NOT_IN_LIST, "Bazzinga", obtainValueToTest(45));
+    }
+
+    private String obtainValueToTest(int value) {
+        return getListToTest().get(value - 1);
+    }
+
+    private List<String> getListToTest() {
+        FizzBuzz fizzBuzz = new FizzBuzz();
+        return fizzBuzz.createOneToHundredList();
+    }
 }
